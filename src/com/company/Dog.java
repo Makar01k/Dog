de9c581c608c72cdf22525aa54a5d0bd49e7cf78
Makar01k <makar.kochetkov@gmail.com>
2022-04-24 22:52:55 +0300

    Initial commit

---
+++ src/com/company/Dog.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class Dog {
+    public static int age;
+    public static String name;
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+
+public class Main {
+
+    String name;
+    int age;
+
+    public static void main(String[] args) {
+        Dog Ben = new Dog();
+        Ben.age = 1;
+        Ben.name = " Ben ";
+        System.out.println("Собака " + Dog.name + ",возрост " +Dog.age);
+
+
+
+        Dog Bas = new Dog();
+        Bas.age = 10;
+        Bas.name = " Bas";
+        System.out.println(" Пес" +Dog.name + ",возрост " +Dog.age);
+
+
+
+
+        }
+
+    }
+
